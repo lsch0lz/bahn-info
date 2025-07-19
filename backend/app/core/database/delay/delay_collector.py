1876921876e3eb806ab2c0cdb87a9f1538ceea59
Lukas Scholz <lukas.scholz@deutschebahn.com>
2025-07-19 11:37:53 +0200

    [backend] feat: basic DelayDataCollector

---
+++ backend/app/core/database/delay/delay_collector.py
@@ -0,0 +1,15 @@
+class DelayDataCollector:
+    async def collect_delay_data(self):
+        pass
+
+    async def collect_station_delays(self):
+        pass
+
+    async def save_delay_records(self):
+        pass
+
+    def start_scheduler(self):
+        pass
+
+    def stop_scheduler(self):
+        pass
